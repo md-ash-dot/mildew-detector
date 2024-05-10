ef7d2a07f331fcead749b53605a7dc376861f437
MD ASH <bma94.7@gmail.com>
2024-05-10 18:45:32 +0000

    add average image size plot to ml performance page

--- app_pages/page_ml_performance.py
+++ app_pages/page_ml_performance.py
@@ -8,6 +8,12 @@ from src.machine_learning.evaluate_clf import load_test_evaluation
 def page_ml_performance_metrics():
     version = 'v1'
 
+    st.write("### Average Image size in dataset")
+
+    average_image_size = plt.imread(f"outputs/{version}/avg_img_size.png")
+    st.image(average_image_size, caption='Average Image Size - Width average: 256 Height average: 256')
+    st.write("---")
+
     st.write("### Train, Validation and Test Set: Labels Frequencies")
 
     labels_distribution = plt.imread(f"outputs/{version}/labels_distribution.png")
