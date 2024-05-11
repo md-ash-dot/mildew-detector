fa250b224f36491fdd53d457e0ebb664da0e9d6b
MD ASH <bma94.7@gmail.com>
2024-05-11 11:17:55 +0000

    Add general information to summary page

--- app_pages/page_summary.py
+++ app_pages/page_summary.py
@@ -8,7 +8,7 @@ def page_summary_body():
 
     st.info(
         f"**General Information**\n"
-        f"* Farmy & Foods, a company in the agricultural sector that produces and harvests "
+        f"* Our client, Farmy & Foods is a company in the agricultural sector that produces and harvests "
         f"different types of food.\n"
         f"* Their cherry plantations have been presenting powdery mildew, "
         f"which is a fungal disease that affects a wide range of plants.\n"
@@ -17,11 +17,15 @@ def page_summary_body():
         f"visually if the leaf tree is healthy or has powdery mildew. If it has powdery mildew, the employee "
         f"applies a specific compound to kill the fungus. The time spent applying this compound is 1 minute. "
         f"The company has thousands of cherry trees located in multiple farms across the country. "
-        f"As a result, this manual process is not scalable due to time spent in the manual process inspection.\n\n"
+        f"As a result, this manual process is not scalable due to time spent in the manual process inspection.\n"
+        f"* To save time in this process, the IT team suggested an ML system that is capable of detecting "
+        f"instantly, using a tree leaf image, if it is healthy or has powdery mildew. A similar manual process "
+        f"is in place for other crops for detecting pests, and if this initiative is successful, there is a  "
+        f"realistic chance to replicate this project to all other crops.\n\n"
         f"**Project Dataset**\n"
         f"* The dataset is a collection of cherry leaf images provided by Farmy & Foods, taken from their crops."
-        f"The available dataset contains 4208 images of \n"
-        f"healthy and powdery mildew containing leaves.")
+        f"The available dataset contains 4208 images of healthy and powdery mildew containing leaves.\n"
+        )
 
     st.write(
         f"* For additional information, please visit and **read** the "
@@ -32,5 +36,5 @@ def page_summary_body():
         f"The project has 2 business requirements:\n"
         f"* 1 - The client is interested in conducting a study to visually differentiate "
         f"a cherry leaf that is healthy from one that contains powdery mildew. \n"
-        f"* 2 - he client is interested in predicting if a cherry tree is healthy or contains powdery mildew. "
+        f"* 2 - The client is interested in predicting if a cherry tree is healthy or contains powdery mildew. "
         )
